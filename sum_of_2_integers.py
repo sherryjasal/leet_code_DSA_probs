d77627c17ba4e76d425beb6ff9252f935fcf026f
RKaur <32834083+sherryjasal@users.noreply.github.com>
2022-07-20 14:55:31 +0530

    Create sum_of_2_integers.py

---
+++ sum_of_2_integers.py
@@ -0,0 +1,7 @@
+## Given two integers a and b, return the sum of the two integers without using the operators + and -.
+## Input: a = 1, b = 2
+## Output: 3
+
+
+def getSum(self, a: int, b: int) -> int:
+    return add(a,b)
