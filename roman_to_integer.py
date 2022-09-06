534e0b8f8247fad591b7d64d4a8ddade95ba4b60
RKaur <32834083+sherryjasal@users.noreply.github.com>
2022-09-06 15:50:23 +0530

    Create roman_to_integer.py

---
+++ roman_to_integer.py
@@ -0,0 +1,31 @@
+## Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+## For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+## Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+##I can be placed before V (5) and X (10) to make 4 and 9. 
+##X can be placed before L (50) and C (100) to make 40 and 90. 
+##C can be placed before D (500) and M (1000) to make 400 and 900.
+##Given a roman numeral, convert it to an integer.
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        translations = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        number = 0
+        s = s.replace("IV","IIII").replace("IX", "VIIII")
+        s = s.replace("XL","XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD","CCCC").replace("CM", "DCCCC")
+        
+        for char in s:
+            number += translations[char]
+        
+        return number
+
+## lessons learnt    
+## hashmaps and strings
+## translations hashmap string -> integers
+## counter
+## conditions for 4,9 , 40,90 and 400,900
+## looping through string
+## counter update using translation hashmap with index number as character
+## return number
